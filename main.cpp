3886cb09cec47719cc51cae8e2f57a8e849106d6
Pranavgulati <pranavgulati95@yahoo.com>
2016-02-16 20:42:12 +0530

    Initial commit
    -added .gitignore

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Attiny85_ECG_PPG.cpp
+ *
+ * Created: 2/16/2016 8:38:54 PM
+ * Author : Pranav
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
